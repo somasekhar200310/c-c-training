d632f6ed9df7662db54c254d685f0d801d312921
somasekhar200310 <somasekhar200310@gmail.com>
2024-10-23 22:05:41 +0530

    Add files via upload

---
+++ arthimetic operators.c
@@ -0,0 +1,70 @@
+Addition (+)
+
+int main() {
+    
+    int a = 10;
+    int b = 20;
+
+    printf("a + b = %d\n", a + b);
+     return 0;
+}
+
+OUTPUT:
+a+b=30
+----------------------------------------------------------------
+Subtraction(-)
+
+
+int main() {
+    
+    int a = 10;
+    int b = 20;
+
+    printf("a - b = %d\n", a - b);
+     return 0;
+}
+
+OUTPUT:
+a-b=-10
+-------------------------------------------------------------------
+Multiplication(*)
+
+int main() {
+    
+    int a = 10;
+    int b = 20;
+
+    printf("a * b = %d\n", a * b);
+     return 0;
+}
+
+OUTPUT:
+a*b=200
+---------------------------------------------------------------------
+Dvision (/)
+
+int main() {
+    
+    int a = 20;
+    int b = 10;
+
+    printf("a / b = %d\n", a / b);
+     return 0;
+}
+
+OUTPUT:
+a/b=2
+---------------------------------------------------------------------
+Modulus(%)
+int main() {
+    
+    int a = 5;
+    int b = 2;
+
+    printf("a % b = %d\n", a % b);
+     return 0;
+}
+
+
+OUTPUT:
+a%b=1
